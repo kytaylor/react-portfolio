436ebfb4f4baa6659080ea6b24252077fe1e019c
kytaylor <ky.taylor5468@gmail.com>
2021-04-08 18:21:52 -0700

    adds header tag to header.js to fix styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,14 @@ import React from "react";
 function Header() {
     return(
         <>
-        <nav>
-            <a class="about-link link-button" href="#about">About</a>
-            <a class="portfolio-link link-button" href="#portfolio">Portfolio</a>
-            <a class="resume-link link-button" href="#resume">Resume</a>
-            <a class="contact-link link-button" href="#contact">Contact</a>
-        </nav>
+        <header>
+            <nav>
+                <a class="about-link link-button" href="#about">About</a>
+                <a class="portfolio-link link-button" href="#portfolio">Portfolio</a>
+                <a class="resume-link link-button" href="#resume">Resume</a>
+                <a class="contact-link link-button" href="#contact">Contact</a>
+            </nav>
+        </header>
         </>
     )
 }
